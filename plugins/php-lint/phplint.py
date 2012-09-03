3dc0b40cf04b6143526700340ec8fde4eab1dd6c
Jan Pecha <janpecha@email.cz>
2012-09-03 12:13:26 +0200

    PhpLint plugin: bug fix
    Signed-off-by: Jan Pecha <janpecha@email.cz>

--- plugins/php-lint/phplint.py
+++ plugins/php-lint/phplint.py
@@ -1,23 +1,23 @@
-#	Copyright (c) 2012 Jan Pecha (http://janpecha.iunas.cz/) All rights reserved.
-#	
-#	Redistribution and use in source and binary forms, with or without modification,
-#	are permitted provided that the following conditions are met:
-#	* Redistributions of source code must retain the above copyright notice, this
-#	  list of conditions and the following disclaimer.
-#	* Redistributions in binary form must reproduce the above copyright notice, this
-#	  list of conditions and the following disclaimer in the documentation and/or
-#	  other materials provided with the distribution.
-#	
-#	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-#	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-#	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-#	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-#	ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-#	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-#	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-#	ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-#	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-#	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# Copyright (c) 2012 Jan Pecha (http://janpecha.iunas.cz/) All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice, this
+#   list of conditions and the following disclaimer in the documentation and/or
+#   other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from gi.repository import GObject, Gtk, Gedit
 import re
@@ -39,7 +39,7 @@ class PhpLintAfterSavePlugin(GObject.Object, Gedit.ViewActivatable):
 	def do_activate(self):
 		"""Activate plugin."""
 		self.doc = self.view.get_buffer()
-		self.handler_id = self.doc.connect("save", self.on_document_saving)
+		self.handler_id = self.doc.connect("saved", self.on_document_saving)
 
 	def do_deactivate(self):
 		"""Deactivate plugin."""
